e6f3466aa6a01a94493d6b2aea5cdf72d0357a43
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-08-23 15:01:31 +0100

    task 11

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -56,4 +56,16 @@ feature 'restaurants' do
     end
   end
 
+  context 'deleting restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+      scenario 'let a user delete a restaurant' do
+        visit '/restaurants'
+        click_link 'Delete KFC'
+        expect(page).not_to have_content('KFC')
+        expect(page).to have_content('Restaurant deleted successfully')
+        expect(current_path).to eq '/restaurants'
+      end
+  end
+
 end
