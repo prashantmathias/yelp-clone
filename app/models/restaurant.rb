0499f37d64ba5ae090205fecc49ff8160f4b16de
prashantmathias <mathiasaudio@gmail.com>
2016-08-24 12:20:29 +0100

    completes section:14

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
 end
