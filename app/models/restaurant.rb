ceefcdecce9f2eeaaf19202873e561c59ada129e
Adam <adamjbrowner@gmail.com>
2016-08-25 13:13:57 +0100

    Finished yelp v3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews, dependent: :destroy
+  # has_many :reviews, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+  belongs_to :user
+  has_many :reviews,
+        -> { extending WithUserAssociationExtension },
+        dependent: :destroy
+  def build_review(attributes = {}, user)
+    review = reviews.build(attributes)
+    review.user = user
+    review
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824144105) do
+ActiveRecord::Schema.define(version: 20160825113441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160824144105) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "restaurant_id"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
@@ -55,4 +56,5 @@ ActiveRecord::Schema.define(version: 20160824144105) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "reviews", "restaurants"
+  add_foreign_key "reviews", "users"
 end
