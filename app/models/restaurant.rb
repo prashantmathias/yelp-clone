7d92eb8afb68909c859cfc15de68f563a2a96a76
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-08-26 13:36:25 +0100

    Implemented thumbnails but not acceptance tests

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,16 @@
 class Restaurant < ActiveRecord::Base
-  # has_many :reviews, dependent: :destroy
+
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
   belongs_to :user
   has_many :reviews,
         -> { extending WithUserAssociationExtension },
         dependent: :destroy
+
+  has_attached_file :image, :style => { :thumb => ["210x210#", :jpg] },
+                    :default_url => "/images/:style/missing.png"
+
+  validates_attachment :image
+
   def build_review(attributes = {}, user)
     review = reviews.build(attributes)
     review.user = user
