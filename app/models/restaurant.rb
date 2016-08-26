4598a0c5fbac83ed0bc54fcf7c8f16b303e4fa5a
prashantmathias <mathiasaudio@gmail.com>
2016-08-26 17:30:35 +0100

    adds changes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,10 +6,8 @@ class Restaurant < ActiveRecord::Base
         -> { extending WithUserAssociationExtension },
         dependent: :destroy
 
-  has_attached_file :image, :style => { :thumb => ["210x210#", :jpg] },
-                    :default_url => "/images/:style/missing.png"
-
-  validates_attachment :image
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def build_review(attributes = {}, user)
     review = reviews.build(attributes)
